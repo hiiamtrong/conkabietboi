e17228796a3eec3f267eb3c5b3a65936d96f02fc
Xuân Trọng <54925853+hiiamtrong@users.noreply.github.com>
2021-05-03 02:14:22 +0700

    :tada: bug/fix-edit-link (#3)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/hiiamtrong/conkabietboi/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
